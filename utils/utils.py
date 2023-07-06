3ff054945d9810c700a83993c21983c05495f08a
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-07-06 22:53:48 +0700

    add function to EXECUTED

--- utils/utils.py
+++ utils/utils.py
@@ -26,3 +26,16 @@ def selection_by_keys(operation):
         if all(key in item.keys() for key in keys_to_check):
             filtered_list.append(item)
     return filtered_list
+
+
+def completed_operation(full_operations):
+    """
+
+    :param full_operations: список операций с полными данными
+    :return:список с выполненными опереациями(EXECUTED)
+    """
+    exe_list = []
+    for item in full_operations:
+        if item['state'] == 'EXECUTED':
+            exe_list.append(item)
+    return exe_list
