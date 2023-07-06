66d51cc748039271857192dbf6fe6c60abf10c62
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-07-06 22:54:15 +0700

    add test completed_operation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,8 @@
 from utils import utils
 
 
+def test_completed_operation():
+    assert utils.completed_operation(
+        [{"id": 441945886, "state": "EXECUTED"}, {"id": 594226727, "state": "CANCELED"}]) == [
+               {"id": 441945886, "state": "EXECUTED"}]
+
